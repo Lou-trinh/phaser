d72aa5db30b790fa0941db1cc9b0d854a881dc7c
cuong <loutrinh2312000@gmail.com>
2025-10-25 09:12:39 +0700

    update click mouse

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -7,36 +7,34 @@ export class Game extends Scene {
     }
 
     preload() {
-        this.load.setPath('assets/spine/goblins/'); 
+        this.load.setPath('assets/spine/goblins/');
 
         this.load.spineAtlas('goblins-atlas', 'player_31_gameplay.atlas');
         this.load.spineJson('goblins', 'player_31_gameplay.json');
     }
 
     create() {
-        const goblin = this.add.spine(550, 630, "goblins", "goblins-atlas", 
+        const goblin = this.add.spine(550, 630, "goblins", "goblins-atlas",
             new SkinsAndAnimationBoundsProvider(null, ["default"])
         );
         goblin.skeleton.setSkinByName("default");
         goblin.animationState.setAnimation(0, 'reload', true);
         goblin.scale = 0.5;
 
-        const button = this.add.text(520, 100, 'change');
+        let reloadTimeout: ReturnType<typeof setTimeout> | null = null;
 
-        let isReload = false;
+        this.input.on('pointerdown', () => {
+            // Hủy timeout cũ nếu đang có
+            if (reloadTimeout !== null) {
+                clearTimeout(reloadTimeout);
+            }
 
-        button.setInteractive();
-        
-        button.on('pointerdown', () => {
-            isReload = !isReload;
-            if (isReload) {
-                goblin.animationState.setAnimation(0, 'shoot', true);
-            } else {
+            goblin.animationState.setAnimation(0, 'shoot', true);
+
+            
+            reloadTimeout = setTimeout(() => {
                 goblin.animationState.setAnimation(0, 'reload', true);
-            } 
+            }, 500);
         });
-        
-        
     }
-
-}   
+}
